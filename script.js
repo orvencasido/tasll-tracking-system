820837b2e429d6de07eb618b1d06af5a3df13887
orvencasido <orvencasidop@gmail.com>
2024-12-18 20:19:26 +0800

    added login

--- script.js
+++ script.js
@@ -1,93 +1,90 @@
-
 var app = new function() {
   this.el = document.getElementById('tasks');
-
   this.tasks = [];
 
-  
-  
+  // Fetch and render all tasks
   this.FetchAll = function() {
     var data = '';
 
     if (this.tasks.length > 0) {
       for (i = 0; i < this.tasks.length; i++) {
           data += '<tr>';
+          // Display the task number
           data += '<td>' + (i + 1) + '</td>';
+          // Display the task text
           data += '<td>' + this.tasks[i] + '</td>';
+          // Add Edit and Delete buttons with the task index
           data += '<td><button onclick="app.Edit(' + i + ')" class="btn btn-warning">Edit</button></td>';
           data += '<td><button onclick="app.Delete(' + i + ')" class="btn btn-danger">Delete</button></td>';
           data += '</tr>';
       }
-  }  
+    } else {
+      // Handle the case when no tasks are available
+      data = '<tr><td colspan="4" class="text-center">No tasks added</td></tr>';
+    }
 
+    // Update the table with task data
     this.Count(this.tasks.length);
     return this.el.innerHTML = data;
   };
 
+  // Add a new task
   this.Add = function () {
-    el = document.getElementById('add-todo');
-    // Get the value
+    var el = document.getElementById('add-todo');
     var task = el.value;
 
     if (task) {
-      // Add the new value
+      // Add the new task to the array
       this.tasks.push(task.trim());
-      // Reset input value
+      // Reset the input value
       el.value = '';
-      // Dislay the new list
+      // Re-render the tasks
       this.FetchAll();
     }
   };
 
+  // Edit an existing task
   this.Edit = function (item) {
     var el = document.getElementById('edit-todo');
-    // Display value in the field
-    el.value = this.tasks[item];
-    // Display fields
+    el.value = this.tasks[item]; // Set the current task text in the input field
     document.getElementById('edit-box').style.display = 'block';
-    self = this;
 
+    var self = this;
     document.getElementById('save-edit').onsubmit = function() {
-      // Get value
       var task = el.value;
-
       if (task) {
-        // Edit value
-        self.tasks.splice(item, 1, task.trim());
-        // Display the new list
-        self.FetchAll();
-        // Hide fields
+        self.tasks.splice(item, 1, task.trim()); // Update the task
+        self.FetchAll(); // Re-render tasks
         CloseInput();
       }
     }
   };
 
+  // Delete a task
   this.Delete = function (item) {
-    // Delete the current row
-    this.tasks.splice(item, 1);
-    // Display the new list
-    this.FetchAll();
+    this.tasks.splice(item, 1); // Remove the task from the array
+    this.FetchAll(); // Re-render tasks
   };
 
+  // Update the task count in the UI
   this.Count = function(data) {
-    var el   = document.getElementById('counter');
+    var el = document.getElementById('counter');
     var name = 'Tasks';
 
     if (data) {
-        if(data ==1){
-            name = 'Task'
-        }
-      el.innerHTML = data + ' ' + name ;
-    } 
-    else {
+      if(data == 1){
+        name = 'Task';
+      }
+      el.innerHTML = data + ' ' + name;
+    } else {
       el.innerHTML = 'No ' + name;
     }
   };
-  
 }
 
-app.FetchAll();
+app.FetchAll(); // Initially fetch and display tasks
 
+// Close the edit box
 function CloseInput() {
   document.getElementById('edit-box').style.display = 'none';
 }
